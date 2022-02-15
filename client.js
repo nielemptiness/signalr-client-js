5d68eef8cc21c69506308ffa1b74f413941d323b
Proniaiev Maksym <m.pronyayev@beter.co>
2022-02-15 23:28:07 +0200

    extend stats

--- client.js
+++ client.js
@@ -1,6 +1,24 @@
 const signalr = require('@microsoft/signalr');
 const dotenv = require('dotenv');
 
+let recievedUpdates = 0;
+let recievedHeartBeats = 0;
+let previousUpdateTime;
+let lastUpdateTime;
+let previousHeartBeatTime;
+let lastHeartBeatTime;
+
+const onExit = () => {
+    console.log('total received updates: ' + recievedUpdates);
+    
+    if (lastUpdateTime && previousUpdateTime)
+        console.log('last update: ' + lastUpdateTime + ' \n previous update ' + previousUpdateTime);
+    
+        console.log('total received heartBeats ' + recievedHeartBeats);
+    
+    if (lastHeartBeatTime && previousHeartBeatTime)
+        console.log('last heartbeat: ' + lastHeartBeatTime + ' \n previous heartbeat ' + previousHeartBeatTime);
+}
 
 dotenv.config({ path: "./test.env" });
 const connection = new signalr.HubConnectionBuilder()
@@ -9,6 +27,7 @@ const connection = new signalr.HubConnectionBuilder()
     skipNegotiation: true,
     transport: signalr.HttpTransportType.WebSockets
 })
+.withAutomaticReconnect([0, 1, 3, 5])
 .build();
 
 connection.onclose(function (e) {
@@ -18,6 +37,7 @@ connection.onclose(function (e) {
     else {
         console.log('Disconnected');
     }
+    onExit();
 });
 
 const processEvt = (event, eventName) => {
@@ -28,9 +48,29 @@ const processEvt = (event, eventName) => {
     }
 };
 
-connection.on("OnUpdate", (evt) => processEvt(evt, 'OnUpdate'));		
-connection.on("OnHeartbeat", (evt) => processEvt(evt, 'OnHeartbeat'));
+const processHeartBeat = (event, eventName) => {
+    processEvt(event, eventName);
+    recievedHeartBeats++;
+    previousHeartBeatTime = lastHeartBeatTime;
+    lastHeartBeatTime = new Date();
+}
+
+const processUpdate = (event, eventName) => {
+    processEvt(event, eventName);
+    recievedUpdates++;
+    previousUpdateTime = lastUpdateTime;
+    lastUpdateTime = new Date();
+};
+
+
+connection.on("OnUpdate", (evt) => processUpdate(evt, 'OnUpdate'));		
+connection.on("OnHeartbeat", (evt) => processHeartBeat(evt, 'OnHeartbeat'));
 
 connection.start()
 .then(() => console.log('connected successfully!'))
 .catch(e => console.error(e));
+
+process.on("SIGINT", () => {
+    onExit();
+    process.exit();
+});
